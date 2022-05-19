047afa0cc2a8547bdb51641462fd011a6e81f758
Alexangel Sanchez <alexangel2323@gmail.com>
2022-05-19 14:37:14 -0500

    commit tree d5148cdb270f312fcd145b03fb968b7e99013afb

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = "Grace Hopper";
+// Using interpolation `${}` we can insert currentUser into the welcomeMessage with ` marks 
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+// excitedWelcomeMessage is just set equal to welcomeMessage and uppercased
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// shortGreeting we want welcome and the first intial of currentUser name followed by ! so we use slice to just keep the first intial 
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
